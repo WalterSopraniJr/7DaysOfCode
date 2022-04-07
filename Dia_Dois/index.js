da04434563a20f914311bdb0aa2902d4a5f1a9f9
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-04-06 23:30:48 -0300

    Desafio dia dois.

---
+++ Dia_Dois/index.js
@@ -0,0 +1,8 @@
+// Desafio do dia 2
+
+var userName = "Walter";
+let idade = 26;
+let liguagemProgramacao = "JavaScript";
+
+console.log(`Olá, ${userName}, você tem ${idade} e já está afiado em ${liguagemProgramacao}.`);
+console.log("Mantenha o ritmo!")
